3b55c4693d284bf80735f3a792bcab2914aa5772
Alexey Shatskikh <shtskkh@gmail.com>
2023-11-14 21:21:24 +0700

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,76 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="login">
+  <div class="login__container">
+    <h3 class="login__title">Вход</h3>
+    <input class="input login__email" type="email" placeholder="Email или логин">
+    <input class="input login__password" type="password" placeholder="Пароль">
+    <button class="btn-primary login__button">Войти</button>
+    <router-link class="login__link" to="/reset">Не можете войти?</router-link>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+.login {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login__container {
+  display: flex;
+  flex-direction: column;
+  padding: 32px 16px;
+  border: 2px solid var(--main-color);
+  border-radius: 8px;
+}
+
+.login__title {
+  text-align: center;
+  margin-bottom: 32px;
+}
+
+.input {
+  display: inline-block;
+  height: 56px;
+  padding: 16px 20px;
+}
+
+.login__email {
+  margin-bottom: 16px;
+}
+
+.login__password {
+  margin-bottom: 32px;
+}
+
+.login__button {
+  height: 56px;
+  font-size: 20px;
+  margin-bottom: 16px;
+}
+
+@media (min-width: 1280px) {
+  .login__container {
+    width: 400px;
+  }
+}
+@media (min-width: 640px) and (max-width: 1279px) {
+  .login__container {
+    width: 400px;
+  }
+}
+@media (max-width: 639px) {
+  .login__container {
+    width: 360px;
+  }
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: '/login'
+    },    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginPage.vue'),
+    },
+  ]
+})
+
+export default router
